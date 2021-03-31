a17bc3a653902ce661f0295d4b3645127d626180
Fuat Güzel <fuatguzeel@gmail.com>
2021-04-01 02:03:53 +0300

    Add google icon

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-import {darkConflowerBlue} from './src/colors.js';
+import {darkConflowerBlue} from './src/Colors.js';
 
 export default function App() {
   return (
@@ -10,8 +10,8 @@ export default function App() {
       <Text style={{ color: '#fff', fontSize: 34, fontWeight: 'bold' }} >Welcome to my App!
       </Text>
       <Image 
-        style={{ width: 300, height: 60, marginTop:259 }}
-        source={require('./src/assets/signin-button.png')} 
+        style={{ width: 60, height: 60, marginTop:259 }}
+        source={require('./src/assets/google-icon.png')} 
       />
       <StatusBar style="auto" />
     </View>
