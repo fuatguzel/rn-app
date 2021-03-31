9e01e1c2bfe89e95a0694d4fd5d45442c74603cb
Fuat Güzel <fuatguzeel@gmail.com>
2021-04-01 01:22:54 +0300

    Add screen file

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+import {darkConflowerBlue} from './src/colors.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: '#fff', fontSize: 34, fontWeight: 'bold' }} >Welcome to my App!
+      </Text>
+      <Image 
+        style={{ width: 300, height: 60, marginTop:259 }}
+        source={require('./src/assets/signin-button.png')} 
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: darkConflowerBlue,
     alignItems: 'center',
     justifyContent: 'center',
   },
