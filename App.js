86b0050db807872911cee1773301eeec5f25718c
Fuat Güzel <fuatguzeel@gmail.com>
2021-04-16 17:45:36 +0300

    add constants

--- App.js
+++ App.js
@@ -2,16 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-import {darkConflowerBlue} from './src/Colors.js';
+import { darkConflowerBlue } from './constants/Colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: '#fff', fontSize: 34, fontWeight: 'bold' }} >Welcome to my App!
+      <Text style={{ color: '#fff', fontSize: 34, fontWeight: 'bold' }}>
+        Welcome to my App!
       </Text>
-      <Image 
-        style={{ width: 60, height: 60, marginTop:259 }}
-        source={require('./src/assets/google-icon.png')} 
+      <Image
+        style={{ width: 60, height: 60, marginTop: 259 }}
+        source={require('./src/assets/google-icon.png')}
       />
       <StatusBar style="auto" />
     </View>
