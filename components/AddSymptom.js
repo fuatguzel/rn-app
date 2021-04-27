bcd9205b646ad98502a6c43930d5a53239d57d7d
Fuat Güzel <fuatguzeel@gmail.com>
2021-04-27 23:18:54 +0300

    update AddSymptom.js

--- components/AddSymptom.js
+++ components/AddSymptom.js
@@ -3,7 +3,20 @@ import { View } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
 
 export default class AddSymptom extends Component {
+  state = {
+    temperature: 0,
+    cough: 0,
+    dizziness: 0,
+    tired: 0,
+    appetite: 0,
+  };
+
+  slide = (metric, value) => {
+    this.setState(() => ({
+      [metric]: value,
+    }));
+  };
   render() {
-    return <View>{getMetricMetaInfo('bike').getIcon}</View>;
+    return <View></View>;
   }
 }
