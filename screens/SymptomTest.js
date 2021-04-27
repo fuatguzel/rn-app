c77616ab28f661b11398fe2c118bab7560aa905c
Fuat Güzel <fuatguzeel@gmail.com>
2021-04-28 02:08:40 +0300

    add redux

--- screens/SymptomTest.js
+++ screens/SymptomTest.js
@@ -3,14 +3,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CustomHeader from '../components/CustomHeader';
 import AddSymptom from '../components/AddSymptom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
 
 import { darkConflowerBlue } from '../constants/Colors';
 
 export default function SymptomTest() {
   return (
-    <View>
-      <AddSymptom />
-    </View>
+    <Provider store={createStore(reducer)}>
+      <View>
+        <AddSymptom />
+      </View>
+    </Provider>
   );
 }
 
